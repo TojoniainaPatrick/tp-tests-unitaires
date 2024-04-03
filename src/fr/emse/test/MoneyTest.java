a64384171712ad33899867d6f70040fd6d5ee220
Tojoniaina Patrick <tojoniainapatrickr@gmail.com>
2024-04-03 06:44:51 +0300

    testSimpleAdd() method implemented on MoneyTest class

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 public class MoneyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSimpleAdd() {
+		Money m12CHF = new Money(12, "CHF");
+		Money m14CHF = new Money(14, "CHF");
+		Money expected = new Money(26, "CHF");
+		Money result = m12CHF.add(m14CHF);
+		assertTrue(expected.equals(result));
 	}
+
 
 }
