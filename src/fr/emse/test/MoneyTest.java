4fec1255de333edce7e2181fdaad1304f08bed4a
Tojoniaina Patrick <tojoniainapatrickr@gmail.com>
2024-04-03 06:46:58 +0300

    testEquals() method implemented on MoneyTest class

--- src/fr/emse/test/MoneyTest.java
+++ src/fr/emse/test/MoneyTest.java
@@ -15,5 +15,15 @@ public class MoneyTest {
 		assertTrue(expected.equals(result));
 	}
 
+	@Test
+	public void testEquals() {
+		Money m12CHF= new Money(12, "CHF");
+		Money m14CHF= new Money(14, "CHF");
+		assertTrue(!m12CHF.equals(null)); 
+		assertEquals(m12CHF, m12CHF);
+		assertEquals(m12CHF, new Money(12, "CHF"));
+		assertTrue(!m12CHF.equals(m14CHF));
+	}
+
 
 }
